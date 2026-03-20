c01b9a4253dee00686e22d00f50a323bc4acb90d
gilman qazi <gilmanqazi@gmail.com>
2026-03-20 20:25:51 +0530

    final controller fixx

--- Frontend/Perplexity-Frontend/src/features/auth/Services/auth.api.js
+++ Frontend/Perplexity-Frontend/src/features/auth/Services/auth.api.js
@@ -45,25 +45,25 @@ return res.data
 }
 }
 
-// export const verifyEmail = async (token)=>{
-//   try{
-// const res = await api.get(`/api/auth/verify-email?token${token}`)
-// console.log(res)
-// return res.data
-//   }catch(err){
-//     console.log(err)
-//     throw err
-//   }
-// }
+export const verifyEmail = async (token)=>{
+  try{
+const res = await api.get(`/api/auth/verify-email?token${token}`)
+console.log(res)
+return res.data
+  }catch(err){
+    console.log(err)
+    throw err
+  }
+}
 
-// export const resendVerification = async (email)=>{
+export const resendVerification = async (email)=>{
 
-//   try{
-//     const res = await api.post("/api/auth/resend-verification-email",{email})
-//     console.log(res)
-//     return res.data
-//   }catch(err){
-//     console.log(err)
-//     throw err
-//   }
-// }
+  try{
+    const res = await api.post("/api/auth/resend-verification-email",{email})
+    console.log(res)
+    return res.data
+  }catch(err){
+    console.log(err)
+    throw err
+  }
+}
